745f0bfc107fb48929d7ef0282c7d5aaf5c6f4e2
grndhmmr <rgruendh@hs-mittweida.de>
2021-05-25 11:38:47 +0200

    nocomment

--- main.c
+++ main.c
@@ -1,18 +1,18 @@
-#in#include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
     //Erzeugung und Öffnen zur Bearbeitung einer Textdatei zur Dokumentation//
 
-        FILE *fh;
+        FILE *fh_ptr;
         const char *pfadzufh;
-        fh=fopen("Beleg_Samba_Logfiles.txt","w+");
+        fh_ptr=fopen("Beleg_Samba_Logfiles.txt","w+");
         pfadzufh="./Beleg_Samba_Logfiles.txt";
 
     //Fehlermeldung, falls die Datei nicht erzeugt bzw. geöffnet werden kann//
 
-        if(fh==NULL)
+        if(fh_ptr==NULL)
         {
             puts("Die Datei kann nicht geöffnet werden!");
             exit(1);
@@ -20,7 +20,7 @@ int main()
 
     //Dateikopf//
 
-        fprintf(fh,"\n\n\n************************************************* Belegarbeit im Modul 'Programmierung 1' *************************************************\n\n\nThema: \t\t\tErstellung eines Programms in der Programmiersprache 'C' zur Auswertung von Samba Logfiles\n\n\n*******************************************************************************************************************************************\n\nBearbeitet durch:\tRafael Gründhammer\t(CC20w1-B)\n\t\t\tAnne Pörs\t\t(CC20w1-B)\n");
+        fprintf(fh_ptr,"\n\n\n************************************************* Belegarbeit im Modul 'Programmierung 1' *************************************************\n\n\nThema: \t\t\tErstellung eines Programms in der Programmiersprache 'C' zur Auswertung von Samba Logfiles\n\n\n*******************************************************************************************************************************************\n\nBearbeitet durch:\tRafael Gründhammer\t(CC20w1-B)\n\t\t\tAnne Pörs\t\t(CC20w1-B)\n");
 
     //Anzeige des Speicherortes der Datei//
 
@@ -44,7 +44,7 @@ int main()
     {
         //Anzeige des Inhalts der Datei 'Beleg_Samba_Logfiles.txt'
 
-            if(fh==NULL)
+            if(fh_ptr==NULL)
             {
             printf("Datei konnte nicht geoeffnet werden.\n");
             }
@@ -53,12 +53,12 @@ int main()
             {
             // komplette Datei zeichenweise ausgeben
                 int lesen;
-            // Bereits deklariert: fh=fopen("./Beleg_Samba_Logfiles.txt","w+");
-                while((lesen=fgetc(fh))!=EOF)
+            // Bereits deklariert: fh_ptr=fopen("./Beleg_Samba_Logfiles.txt","w+");
+                while((lesen=fgetc(fh_ptr))!=EOF)
                 {
                 putchar(lesen);
                 }
-            fclose(fh);
+            fclose(fh_ptr);
             printf("\n");
             return (0);
             }
@@ -73,6 +73,6 @@ int main()
 
 
 
-    fclose(fh);
+    fclose(fh_ptr);
     return 0;
 }
